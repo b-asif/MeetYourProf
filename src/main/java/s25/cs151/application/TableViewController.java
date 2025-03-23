63a669ec33bf7acdcf2735b4eb2cd7767be4368a
JaydenManning <jmanning0805@gmail.com>
2025-03-22 17:34:34 -0700

    change to only 4 digit years accepted in OfficeHourController

--- src/main/java/s25/cs151/application/TableViewController.java
+++ src/main/java/s25/cs151/application/TableViewController.java
@@ -4,12 +4,15 @@ import javafx.beans.property.Property;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
@@ -51,6 +54,19 @@ public class TableViewController  {
 
         tableView.setItems(officehourslist);
     }
+    @FXML
+    private void goToOfficeHour() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("office_hour.fxml"));
+            Parent root = loader.load();
+            OfficeHourController control = loader.getController();
+            control.setStage(stage);
+            stage.getScene().setRoot(root);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private void loadTable() {
         String query = "SELECT * FROM office_hours " + "ORDER BY year DESC, " +
@@ -60,7 +76,7 @@ public class TableViewController  {
                 "WHEN 'SUMMER' THEN 3 " +
                 "WHEN 'SPRING' THEN 4 " +
                 "END ASC;";
-        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/bushraasif/IdeaProjects/MeetYourProf/office_hours.db");
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:./office_hours.db");
              Statement stmt = conn.createStatement();
              ResultSet result = stmt.executeQuery(query)) {
                  while(result.next()) {
